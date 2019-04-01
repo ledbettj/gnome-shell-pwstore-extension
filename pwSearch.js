1c5d3c7ee8f9e5cfc3a0d604e7b0d3a120d58dc9
John Ledbetter <john@callrail.com>
2019-04-01 15:52:41 -0400

    Fix FuzzySearch

--- pwSearch.js
+++ pwSearch.js
@@ -3,11 +3,23 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me             = ExtensionUtils.getCurrentExtension();
 const JaroWinkler    = Me.imports.jaroWinkler.JaroWinkler;
 
-var LiteralSearch = class LiteralSearch {
-
-  search(terms, entries, cancellable) {
+class Searcher {
+  computeTerms(terms) {
     let lcaseTerms  = terms.map( (term) => term.toLowerCase() ),
-        searchTerms = [terms.join('/'), terms.join(' ')],
+        searchTerms = [terms.join('/'), terms.join(' ')];
+
+    /* don't do extra work if the terms are the same. */
+    if (searchTerms[0] == searchTerms[1]) {
+      searchTerms.shift();
+    }
+
+    return searchTerms;
+  }
+};
+
+var LiteralSearch = class LiteralSearch extends Searcher {
+  search(terms, entries, cancellable) {
+    let searchTerms = this.computeTerms(terms),
         results     = [];
 
     entries.some((entry, index) => {
@@ -23,20 +35,19 @@ var LiteralSearch = class LiteralSearch {
 };
 
 
-var FuzzySearch = class FuzzySearch {
-  constuctor(threshold) {
+var FuzzySearch = class FuzzySearch extends Searcher {
+  constructor(threshold) {
+    super();
     this._threshold = threshold || 0.80;
   }
 
   search(terms, entries, cancellable) {
-    let lcaseTerms  = terms.map( (term) => term.toLowerCase() ),
-        searchTerms = [terms.join('/'), terms.join(' ')],
+    let searchTerms = this.computeTerms(terms),
         results     = [];
 
     entries.some((entry, index) => {
       searchTerms.forEach((term) => {
-        let score = JaroWinkler.distance(term, entries.search);
-
+        let score = JaroWinkler.distance(term, entry.search);
         if (score >= this._threshold) {
           results.push({ id: index, score: score });
         }
@@ -45,7 +56,7 @@ var FuzzySearch = class FuzzySearch {
       return cancellable && cancellable.is_cancelled();
     });
 
-    results.sort((a, b) => a.score < b.score ? -1 : a.score > b.score ? 1 : 0);
+    results.sort((a, b) => a.score < b.score ? 1 : a.score > b.score ? -1 : 0);
     results = results.map((e) => e.id);
 
     return results;
