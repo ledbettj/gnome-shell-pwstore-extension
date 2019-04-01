657cdc7b9f002a2a4ea7215628c789c97802475d
John Ledbetter <john@callrail.com>
2019-04-01 14:10:50 -0400

    more js-doc

--- passLauncher.js
+++ passLauncher.js
@@ -1,7 +1,14 @@
 // -*- mode: js; js-indent-level: 2;-*-
 const GLib = imports.gi.GLib;
 
+/**
+ * Handle launching the pass program to copy a password to the clipboard.
+ */
 var PassLauncher = class PassLauncher {
+  /**
+   * Invoke pass -c with the given path.
+   * @param {string} path - password store object to grab, e.g. "Work/email".
+   */
   launch(path) {
     GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
       GLib.spawn_async(null,
--- pwList.js
+++ pwList.js
@@ -2,7 +2,17 @@
 const Gio     = imports.gi.Gio;
 const Signals = imports.signals;
 
+/**
+ * Loads and stores all the entries in the password store, and watches for
+ * updates.
+ */
 var PwList = class PwList {
+  /**
+   * Insantiate a new PwList.
+   * @constructor
+   * @param {string} path - path to the password-store repo, normally
+   *   ~/.password-store/
+   */
   constructor(path) {
     this._passDir = Gio.File.new_for_path(path);
 
@@ -14,12 +24,21 @@ var PwList = class PwList {
     this._mon.connect('changed', this._onDirectoryEvent.bind(this));
   }
 
+  /**
+   * Grab the current list of Password Store entries.
+   * @return {Array}
+   */
   entries() {
     return this._entries;
   }
 
-  /* Invoked when the .password-store directory is updated;
+  /**
+   * Invoked when the .password-store directory is updated;
    * when fired we might need to re-read the list of pass files.
+   * @param {Gio.FileMonitor} _monitor
+   * @param {Gio.File} _file
+   * @param {Gio.File} _otherFile
+   * @param {Gio.FileMonitorEvent} eventType
    */
   _onDirectoryEvent(_monitor, _file, _otherFile, eventType) {
     switch(eventType) {
@@ -35,7 +54,11 @@ var PwList = class PwList {
     }
   }
 
-  /* Read all the files stored under `dir` and add any .gpg files to the entry list */
+  /**
+   * Read all the files stored under `dir` and add any .gpg files to the
+   * entry list.
+   * @param {Gio.File} dir
+   */
   _readEntries(dir) {
     let walker = dir.enumerate_children("standard::*", Gio.FileQueryInfoFlags.NONE, null);
     let info   = null;
@@ -51,6 +74,10 @@ var PwList = class PwList {
     }
   }
 
+  /**
+   * Add the given file to the entries list.
+   * @param {Gio.File} file
+   */
   _addEntry(file) {
     let relative = this._passDir.get_relative_path(file).replace(/\.gpg$/i, '')
     let entry = {
@@ -63,6 +90,11 @@ var PwList = class PwList {
     this._entries.push(entry);
   }
 
+  /**
+   * Check if the given file is a .gpg file.
+   * @param {Gio.File} file
+   * @return {Boolean}
+   */
   _isGpgFile(file) {
     return file.get_basename().toLowerCase().endsWith('.gpg');
   }
